9c3712505901a8e3b8237d024fe8d85b14bd5227
James Doc <jamesdoc@gmail.com>
2018-06-27 14:47:49 +0100

    Fix: guide multiline descriptions line break nicely in API

--- app/serializers/full_guide_serializer.rb
+++ app/serializers/full_guide_serializer.rb
@@ -3,4 +3,9 @@ class FullGuideSerializer < ActiveModel::Serializer
   attributes :guide_name, :slug, :teaser, :description, :copyright, :highlight_first
 
   has_many :studies
+
+  def description
+    object.description.gsub! "\r\n\r\n", "\r\n"
+    object.description.split("\r\n")
+  end
 end
