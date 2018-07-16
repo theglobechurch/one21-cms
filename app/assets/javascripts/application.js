c33d86b30a6a90445869f0f87abdf2acbfc103b1
James Doc <jamesdoc@gmail.com>
2018-07-16 09:17:21 +0100

    Add expandable fields to guide and study forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,25 @@ import renderOne21QuestionForm from './react/questions';
 import renderImageUploader from './react/image_uploader';
 import renderSortable from './sorter';
 import renderStateSwitcher from './react/state_switcher';
+import textareaExpander from './textarea_exander';
 
 renderScripturePicker('.js-scripturePicker');
 renderOne21QuestionForm('.js-one21QuestionForm');
 renderImageUploader('.js-imageUploader');
 renderSortable('.js-sortable');
 renderStateSwitcher('.js-stateSwitcher');
+
+const expandableTA = document.querySelectorAll('.js-expandableTextArea');
+if (expandableTA.length >= 1) {
+  for (let i = 0; i < expandableTA.length; i++) {
+    if (expandableTA[i].tagName.toLowerCase() !== 'textarea') { continue; }
+    
+    textareaExpander(expandableTA[i]);
+
+    expandableTA[i].addEventListener(
+      'keydown',
+      () => { textareaExpander(expandableTA[i]) },
+      false
+    );
+  }
+}
