e582427cad63fcc1fa701c2f07bd0ab5884aedd1
James Doc <jamesdoc@gmail.com>
2018-08-01 16:56:43 +0100

    Allow multiple passages to be selected in a study

--- app/assets/javascripts/react/scripture_picker/index.js
+++ app/assets/javascripts/react/scripture_picker/index.js
@@ -1,145 +1,48 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import PropTypes from 'prop-types';
-import books from './books.json';
-
-const scriptureRefTemplate = {
-  "reference_book": "",
-  "reference_book_start_ch": "",
-  "reference_book_start_v": "",
-  "reference_book_end_ch": "",
-  "reference_book_end_v": ""
-}
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import PropTypes from "prop-types";
+import Picker from "./picker";
 
 class ScripturePicker extends Component {
-
   constructor(props) {
     super(props);
 
-    let ref;
-    const refClone = JSON.parse(JSON.stringify(scriptureRefTemplate));
-
-    if (props.refJson.length === 0) { ref = refClone }
-    else                            { ref = props.refJson[0]; }
+    let t = [];
+    if (props.refText !== "") {
+      t = props.refText.split(",");
+    }
 
     this.state = {
       popupOpen: false,
-      refJson: ref,
-      refText: props.refText,
-      selectedBookData: null,
-      suggestedBooks: []
-    }
+      refJson: props.refJson,
+      refText: t
+    };
   }
 
   onTogglePopup(event) {
-    if (event) { event.preventDefault(); }
+    if (event) {
+      event.preventDefault();
+    }
     this.setState({ popupOpen: !this.state.popupOpen });
   }
 
-  onSaveReference(event) {
+  onPassageSelect(newRefText, newRefJson) {
+    const { refText, refJson } = this.state;
+    refText.push(newRefText);
+    refJson.push(newRefJson);
+
+    this.setState({
+      refText,
+      refJson
+    });
+  }
+
+  onConfirm(event) {
     event.preventDefault();
-
-    // produce JSON and Text
-    const refJson = {
-      "reference_book": document.querySelector('.js-sp-book-name').value,
-      "reference_book_start_ch": document.querySelector('.js-sp-start-ch').value,
-      "reference_book_start_v": document.querySelector('.js-sp-start-v').value,
-      "reference_book_end_ch": document.querySelector('.js-sp-end-ch').value,
-      "reference_book_end_v": document.querySelector('.js-sp-end-v').value
-    }
-
-    const refText = refJson.reference_book
-      + " "
-      + refJson.reference_book_start_ch
-      + ":"
-      + refJson.reference_book_start_v
-      + "–"
-      + refJson.reference_book_end_ch
-      + ":"
-      + refJson.reference_book_end_v;
-
-    this.setState({ refJson, refText });
-    
-    // Send it back and close the popup
-    this.props.onConfirm(refJson, refText);
+    const { refJson } = this.state;
+    this.props.onConfirm(refJson);
     this.onTogglePopup();
-  }
-
-  onBookLookup(event) {
-    const bookEntry = event.target.value;
-    const refJson = this.state.refJson;
-    const suggestedBooks = books.filter(
-      book => book.fullName.toLowerCase().includes(
-        bookEntry.toLowerCase()
-      )
-    );
-
-    if (suggestedBooks.length === 0) {
-      event.target.value = event.target.value.slice(0, -1);
-    }
-
-    if (suggestedBooks.length > 5) {
-      suggestedBooks.length = 5;
-    }
-
-    this.setState({ suggestedBooks });
-  }
-
-  onSelectBook(selectedBook) {
-    const refJson = this.state.refJson;
-    const fieldBookName = document.querySelector('.js-sp-book-name');
-    const fieldStartChapter = document.querySelector('.js-sp-start-ch');
-    const fieldEndChapter = document.querySelector('.js-sp-end-ch');
-    const selectedBookData = books.filter(
-      book => book.fullName.toLowerCase().includes(
-        selectedBook.fullName.toLowerCase()
-      )
-    )[0];
-
-    fieldBookName.value = selectedBook.fullName;
-    fieldStartChapter.max = selectedBook.chapters.length
-    fieldEndChapter.max = selectedBook.chapters.length
-
-    refJson.reference_book = selectedBook.fullName;
-
-    this.setState({ suggestedBooks: [], refJson, selectedBookData });
-  }
-
-  onSelectStartChapter(event) {
-    const selectedChapter = event.target.value;
-    const refJson = this.state.refJson;
-    const fieldStartVerse = document.querySelector('.js-sp-start-v');
-    const fieldEndChapter = document.querySelector('.js-sp-end-ch');
-    const fieldEndVerse = document.querySelector('.js-sp-end-v');
-
-    // Don't allow text entry above the max…
-    if (selectedChapter > this.state.selectedBookData.chapters.length) {
-      event.target.value = event.target.value.slice(0, -1);
-      return;
-    }
-
-    fieldEndChapter.min = selectedChapter;
-    if (fieldEndChapter.value < selectedChapter) {
-      fieldEndChapter.value = selectedChapter;
-    }
-
-    // Arrays are 0 indexed
-    const chapterArrRef = selectedChapter - 1;
-    const verseCount = this.state.selectedBookData.chapters[chapterArrRef];
-
-    fieldStartVerse.max = verseCount
-    if (fieldStartVerse.value > verseCount) {
-      fieldStartVerse.value = verseCount
-    }
-
-    fieldEndVerse.max = verseCount;
-    if (fieldEndVerse.value > verseCount) {
-      fieldEndVerse.value = verseCount
-    }
-
-    refJson.reference_book_start_ch = selectedChapter;
-    this.setState(refJson);
   }
 
   render() {
@@ -149,84 +52,32 @@ class ScripturePicker extends Component {
           className="scripturePicker__preview"
           onClick={this.onTogglePopup.bind(this)}
         >
-          {this.state.refText}
+          {this.state.refText.join(", ")}
         </div>
 
         <ReactCSSTransitionGroup
           transitionName="r_popupFade"
           transitionEnterTimeout={250}
-          transitionLeaveTimeout={250}>
-          { this.state.popupOpen &&
+          transitionLeaveTimeout={250}
+        >
+          {this.state.popupOpen && (
             <div className="r_popup">
+              <Picker onConfirm={this.onPassageSelect.bind(this)} />
 
-              <h1 className="r_popup__title">
-                Bible reference:
-              </h1>
+              {this.state.refText.length > 0 && (
+                <ul>
+                  {this.state.refText.map((ref, i) => <li key={i}>{ref}</li>)}
+                </ul>
+              )}
 
-              <div className="scripturePicker__bookSelector">
-                <input
-                  type="text"
-                  className="js-sp-book-name r_popup__input"
-                  autoFocus
-                  onKeyDown={this.onBookLookup.bind(this)}
-                  defaultValue={this.state.refJson.reference_book}
-                />
-
-                {this.state.suggestedBooks.length !== 0 &&
-                  <ul className="scripturePicker__autocomplete">
-                    {this.state.suggestedBooks.map((book, i) => (
-                      <li
-                        key={i}
-                        onClick={this.onSelectBook.bind(this, book)}
-                      >
-                        {book.fullName}
-                      </li>
-                    ))}
-                  </ul>
-                }
-              </div>
-              
-              <div className="scripturePicker__refSelector">
-                <div className="scripturePicker__refSelector__chv">
-                  <input
-                    type="number"
-                    className="r_popup__input r_popup__input--number js-sp-start-ch"
-                    min="1"
-                    defaultValue={this.state.refJson.reference_book_start_ch}
-                    onChange={this.onSelectStartChapter.bind(this)}
-                  />
-                  v
-                  <input
-                    type="number"
-                    min="1"
-                    className="r_popup__input r_popup__input--number js-sp-start-v"
-                    defaultValue={this.state.refJson.reference_book_start_v}
-                  />
-                </div>
-
-                <div className="scripturePicker__refSelector__chv">
-                  <input
-                    type="number"
-                    min="1"
-                    className="r_popup__input r_popup__input--number js-sp-end-ch"
-                    defaultValue={this.state.refJson.reference_book_end_ch}
-                  />
-                  v
-                  <input
-                    type="number"
-                    min="1"
-                   className="r_popup__input r_popup__input--number js-sp-end-v"
-                    defaultValue={this.state.refJson.reference_book_end_v}
-                  />
-                </div>
-              </div>
-
-              <button
-                onClick={this.onSaveReference.bind(this)}
-                className="btn btn--primary r_popup__actionBtn"
-              >
-                Select
-              </button>
+              {this.state.refText.length > 0 && (
+                <button
+                  onClick={this.onConfirm.bind(this)}
+                  className="btn btn--primary r_popup__actionBtn"
+                >
+                  Confirm
+                </button>
+              )}
 
               <button
                 className="r_popup__btn--close"
@@ -235,7 +86,7 @@ class ScripturePicker extends Component {
                 Cancel
               </button>
             </div>
-          }
+          )}
         </ReactCSSTransitionGroup>
       </div>
     );
@@ -246,30 +97,34 @@ ScripturePicker.propTypes = {
   refJson: PropTypes.array,
   refText: PropTypes.string,
   onConfirm: PropTypes.func.isRequired
-}
+};
 
 export default function(selector) {
   const container = document.querySelector(selector);
 
-  if (!container) { return; }
+  if (!container) {
+    return;
+  }
 
   function callback(refJSON) {
     // Temp hack… go back to edit to allow multiple passages
     const field = document.getElementById(container.dataset.inputid);
-    const references = [];
-    references.push(refJSON);
-    field.value = JSON.stringify(references);
+    field.value = JSON.stringify(refJSON);
   }
 
   let refJson = container.dataset.refjson;
-  try        { refJson = JSON.parse(refJson); }
-  catch(err) { refJson = []; }
+  try {
+    refJson = JSON.parse(refJson);
+  } catch (err) {
+    refJson = [];
+  }
 
   ReactDOM.render(
     <ScripturePicker
-      refJson={refJson} 
-      refText={container.dataset.reftext} 
+      refJson={refJson}
+      refText={container.dataset.reftext}
       onConfirm={callback}
-    />
-  , container);
+    />,
+    container
+  );
 }
---
+++ app/assets/javascripts/react/scripture_picker/picker.js
@@ -0,0 +1,207 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import books from "./books.json";
+
+const scriptureRefTemplate = {
+  reference_book: "",
+  reference_book_start_ch: "",
+  reference_book_start_v: "",
+  reference_book_end_ch: "",
+  reference_book_end_v: ""
+};
+
+export default class Picker extends Component {
+  constructor(props) {
+    super(props);
+
+    const ref = JSON.parse(JSON.stringify(scriptureRefTemplate));
+
+    this.state = {
+      selectedBookData: null,
+      suggestedBooks: [],
+      refJson: ref
+    };
+  }
+
+  onLookupPassage(event) {
+    event.preventDefault();
+
+    const bkName = document.querySelector(".js-sp-book-name");
+    const chStart = document.querySelector(".js-sp-start-ch");
+    const vStart = document.querySelector(".js-sp-start-v");
+    const chEnd = document.querySelector(".js-sp-end-ch");
+    const vEnd = document.querySelector(".js-sp-end-v");
+
+    if (!bkName.value) {
+      return;
+    }
+
+    const refJson = {
+      reference_book: bkName.value,
+      reference_book_start_ch: chStart.value,
+      reference_book_start_v: vStart.value,
+      reference_book_end_ch: chEnd.value,
+      reference_book_end_v: vEnd.value
+    };
+
+    // Empty the form– not needed any more
+    bkName.value = null;
+    chStart.value = null;
+    vStart.value = null;
+    chEnd.value = null;
+    vEnd.value = null;
+
+    const refText = `${refJson.reference_book} ${
+      refJson.reference_book_start_ch
+    }:${refJson.reference_book_start_v}-${refJson.reference_book_end_ch}:${
+      refJson.reference_book_end_v
+    }`;
+
+    // Look up passage from API…
+
+    // Send it back
+    this.props.onConfirm(refText, refJson);
+  }
+
+  onBookLookup(event) {
+    const bookEntry = event.target.value;
+    const suggestedBooks = books.filter(book =>
+      book.fullName.toLowerCase().includes(bookEntry.toLowerCase())
+    );
+
+    if (suggestedBooks.length === 0) {
+      event.target.value = event.target.value.slice(0, -1);
+    }
+
+    if (suggestedBooks.length > 5) {
+      suggestedBooks.length = 5;
+    }
+
+    this.setState({ suggestedBooks });
+  }
+
+  onSelectBook(selectedBook) {
+    const refJson = this.state.refJson;
+    const fieldBookName = document.querySelector(".js-sp-book-name");
+    const fieldStartChapter = document.querySelector(".js-sp-start-ch");
+    const fieldEndChapter = document.querySelector(".js-sp-end-ch");
+    const selectedBookData = books.filter(book =>
+      book.fullName.toLowerCase().includes(selectedBook.fullName.toLowerCase())
+    )[0];
+
+    fieldBookName.value = selectedBook.fullName;
+    fieldStartChapter.max = selectedBook.chapters.length;
+    fieldEndChapter.max = selectedBook.chapters.length;
+
+    refJson.reference_book = selectedBook.fullName;
+
+    this.setState({ suggestedBooks: [], refJson, selectedBookData });
+  }
+
+  onSelectStartChapter(event) {
+    const selectedChapter = event.target.value;
+    const refJson = this.state.refJson;
+    const fieldStartVerse = document.querySelector(".js-sp-start-v");
+    const fieldEndChapter = document.querySelector(".js-sp-end-ch");
+    const fieldEndVerse = document.querySelector(".js-sp-end-v");
+
+    // Don't allow text entry above the max…
+    if (selectedChapter > this.state.selectedBookData.chapters.length) {
+      event.target.value = event.target.value.slice(0, -1);
+      return;
+    }
+
+    fieldEndChapter.min = selectedChapter;
+    if (fieldEndChapter.value < selectedChapter) {
+      fieldEndChapter.value = selectedChapter;
+    }
+
+    // Arrays are 0 indexed
+    const chapterArrRef = selectedChapter - 1;
+    const verseCount = this.state.selectedBookData.chapters[chapterArrRef];
+
+    fieldStartVerse.max = verseCount;
+    if (fieldStartVerse.value > verseCount) {
+      fieldStartVerse.value = verseCount;
+    }
+
+    fieldEndVerse.max = verseCount;
+    if (fieldEndVerse.value > verseCount) {
+      fieldEndVerse.value = verseCount;
+    }
+
+    refJson.reference_book_start_ch = selectedChapter;
+    this.setState(refJson);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="r_popup__title">Bible reference:</h1>
+        <div className="scripturePicker__bookSelector">
+          <input
+            type="text"
+            className="js-sp-book-name r_popup__input"
+            autoFocus
+            onKeyDown={this.onBookLookup.bind(this)}
+            defaultValue={this.state.refJson.reference_book}
+          />
+
+          {this.state.suggestedBooks.length !== 0 && (
+            <ul className="scripturePicker__autocomplete">
+              {this.state.suggestedBooks.map((book, i) => (
+                <li key={i} onClick={this.onSelectBook.bind(this, book)}>
+                  {book.fullName}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="scripturePicker__refSelector">
+          <div className="scripturePicker__refSelector__chv">
+            <input
+              type="number"
+              className="r_popup__input r_popup__input--number js-sp-start-ch"
+              min="1"
+              defaultValue={this.state.refJson.reference_book_start_ch}
+              onChange={this.onSelectStartChapter.bind(this)}
+            />
+            v
+            <input
+              type="number"
+              min="1"
+              className="r_popup__input r_popup__input--number js-sp-start-v"
+              defaultValue={this.state.refJson.reference_book_start_v}
+            />
+          </div>
+
+          <div className="scripturePicker__refSelector__chv">
+            <input
+              type="number"
+              min="1"
+              className="r_popup__input r_popup__input--number js-sp-end-ch"
+              defaultValue={this.state.refJson.reference_book_end_ch}
+            />
+            v
+            <input
+              type="number"
+              min="1"
+              className="r_popup__input r_popup__input--number js-sp-end-v"
+              defaultValue={this.state.refJson.reference_book_end_v}
+            />
+          </div>
+        </div>
+        <button
+          onClick={this.onLookupPassage.bind(this)}
+          className="btn r_popup__actionBtn scripturePicker__refSelector__lookup"
+        >
+          Lookup
+        </button>
+      </div>
+    );
+  }
+}
+
+Picker.propTypes = {
+  onConfirm: PropTypes.func.isRequired
+};
