3eed906d9b8e53baac13f5c7dc1fa35c93ea9cb0
James Doc <jamesdoc@gmail.com>
2018-08-02 15:43:55 +0100

    Attempt to improve dev loading times
    First load of static assets is _slow_. Seems to be a result of asset
    pipeline needing to generate everything from scratch.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,14 +3,27 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.church
-      @church = current_user.church
+    if current_user.churches_id
+      @user = current_user
+      @church = church
       @guides = @church.guides.not_deleted
     else
       # If no church setup then redirect to set one up…
       notice = "You must create your church before proceeding"
       redirect_to new_church_url, notice: notice
     end
+  end
+
+private
+  def church
+    @church ||= Church.find(current_user.churches_id)
+  end
+
+  def guides
+    @guides ||= Guide.
+                not_deleted.
+                includes(:churches).
+                where(churches: {id: @church.id})
   end
 
 end
