cbf5bbee07ccc0eecb71103d2e46a3d8565a42c8
James Doc <jamesdoc@gmail.com>
2018-08-22 11:16:43 +0100

    Tidy up image uploader

--- app/assets/javascripts/react/image_uploader/index.js
+++ app/assets/javascripts/react/image_uploader/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import PropTypes from 'prop-types'
-import ImagePreview from './image_preview';
-import Gallery from './gallery';
-import Toast from '../toast';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import PropTypes from "prop-types";
+import ImagePreview from "./image_preview";
+import Gallery from "./gallery";
+import Toast from "../toast";
 
 class ImageUploader extends Component {
-
   constructor(props) {
     super(props);
     this.dragCounter = 0;
@@ -20,12 +19,12 @@ class ImageUploader extends Component {
       popup: null,
       uploadProgress: null,
       previewURL: p
-    }
+    };
   }
 
   handleDragEnter(e) {
     if (this.dragCounter == 0) {
-      e.target.classList.add('is-hovering');
+      e.target.classList.add("is-hovering");
       e.target.style.backgroundImage = null;
       this.setState({
         errors: [],
@@ -38,7 +37,7 @@ class ImageUploader extends Component {
   handleDragExit(e) {
     this.dragCounter--;
     if (this.dragCounter == 0) {
-      e.target.classList.remove('is-hovering');
+      e.target.classList.remove("is-hovering");
     }
   }
 
@@ -55,16 +54,27 @@ class ImageUploader extends Component {
     let f = undefined;
 
     this.dragCounter = 0;
-    e.target.classList.remove('is-hovering');
+    e.target.classList.remove("is-hovering");
 
-    if (e.dataTransfer !== undefined) { f = e.dataTransfer.files[0]; }
-    if (target.files !== undefined)   { f = target.files[0]; }
-    if (f === undefined)              { return; }
+    if (e.dataTransfer !== undefined) {
+      f = e.dataTransfer.files[0];
+    }
+    if (target.files !== undefined) {
+      f = target.files[0];
+    }
+    if (f === undefined) {
+      return;
+    }
 
     function validateType(f, validTypes) {
       return new Promise((resolve, reject) => {
         if (!f.type.match(`(?:${validTypes})`)) {
-          reject(`File uploaded must be one of [${validTypes.replace('|', ', ')}] but was ${f.type}`)
+          reject(
+            `File uploaded must be one of [${validTypes.replace(
+              "|",
+              ", "
+            )}] but was ${f.type}`
+          );
         } else {
           resolve(true);
         }
@@ -76,58 +86,59 @@ class ImageUploader extends Component {
         const preview = new FileReader();
         preview.readAsDataURL(image);
 
-        preview.onload = function (pr) {
+        preview.onload = function(pr) {
           const dimension_test = new Image();
           dimension_test.src = pr.target.result;
 
-          dimension_test.onload = function () {
+          dimension_test.onload = function() {
             if (this.width < 1024 || this.height < 768) {
-              reject(`Image must be bigger than 1024x768px, but instead was ${this.width}x${this.height}px.`)
+              reject(
+                `Image must be bigger than 1024x768px, but instead was ${
+                  this.width
+                }x${this.height}px.`
+              );
             } else {
               resolve(true);
             }
           };
-        }
+        };
       });
     }
 
     validateType(f, this.props.validTypes)
       .then(res => {
         // If we're sending up an image, make sure it is a valid size
-        if (this.props.validTypes.indexOf('image') !== -1) {
+        if (this.props.validTypes.indexOf("image") !== -1) {
           return validateImagePxSize(f);
         }
       })
       .then(res => {
         this.uploadingImage = f;
-        this.setState({ popup: 'upload' });
+        this.setState({ popup: "upload" });
       })
       .catch(error => {
         this.setState({
           errors: this.state.errors.concat(error)
         });
       });
-
-
   }
 
   confirmUpload(title) {
-
     // Update state with upload progress
     this.setState({ uploadProgress: "Uploading and processing…" });
 
     const data = new FormData();
-    const uid = ['fileupload', (new Date()).getTime(), 'raw'].join('-');
+    const uid = ["fileupload", new Date().getTime(), "raw"].join("-");
 
-    data.append('upload_type', this.props.uploadType);
-    data.append('attachment[file]', this.uploadingImage);
-    data.append('attachment[name]', this.uploadingImage.name);
-    data.append('attachment[uid]', uid);
-    data.append('attachment[graphic_name]', title);
+    data.append("upload_type", this.props.uploadType);
+    data.append("attachment[file]", this.uploadingImage);
+    data.append("attachment[name]", this.uploadingImage.name);
+    data.append("attachment[uid]", uid);
+    data.append("attachment[graphic_name]", title);
 
     const graphicUpload = fetch("/graphics", {
-      credentials: 'include',
-      method: 'POST',
+      credentials: "include",
+      method: "POST",
       body: data,
       cache: "no-store"
     });
@@ -135,18 +146,17 @@ class ImageUploader extends Component {
     graphicUpload
       .then(res => {
         if (res.status > 300) {
-          return res.json()
-            .then(json => {
-              const err = new Error(json[0]);
-              err.res = res;
-              throw err;
-            });
+          return res.json().then(json => {
+            const err = new Error(json[0]);
+            err.res = res;
+            throw err;
+          });
         }
 
         return res.json();
       })
       .then(json => {
-        const file_id = json['id'];
+        const file_id = json["id"];
 
         // Set field value to image
         this.props.onConfirm(file_id);
@@ -154,24 +164,23 @@ class ImageUploader extends Component {
         // Clear any settings
         this.uploadingImage = null;
         this.setState({
-          uploadProgress: 'Complete',
+          uploadProgress: "Complete",
           errors: [],
           popup: null,
-          previewURL: json['images']['960']
+          previewURL: json["images"]["960"]
         });
-
       })
       .catch(err => {
         this.setState({
           errors: this.state.errors.concat(err.message),
-          uploadProgress: 'Upload failed'
+          uploadProgress: "Upload failed"
         });
       });
   }
 
   confirmGallerySelection(id, preview) {
     this.setState({
-      uploadProgress: 'Complete',
+      uploadProgress: "Complete",
       errors: [],
       popup: null,
       previewURL: preview
@@ -191,37 +200,35 @@ class ImageUploader extends Component {
 
   selectExisting(e) {
     e.preventDefault();
-    this.setState({ popup: 'gallery' });
+    this.setState({ popup: "gallery" });
   }
 
   onClosePopup(e) {
-    if (e) { e.preventDefault(); }
+    if (e) {
+      e.preventDefault();
+    }
     this.setState({ popup: null });
   }
 
   render() {
-
     return (
       <div className="imageUpload">
-
         <ReactCSSTransitionGroup
           transitionName="r_popupFade"
           transitionEnterTimeout={250}
-          transitionLeaveTimeout={250}>
-          { this.state.popup && (
+          transitionLeaveTimeout={250}
+        >
+          {this.state.popup && (
             <div className="r_popup">
-
-              { this.state.popup === 'upload' && (
-
+              {this.state.popup === "upload" && (
                 <ImagePreview
                   onConfirm={this.confirmUpload.bind(this)}
                   image={this.uploadingImage}
                   uploadProgress={this.state.uploadProgress}
                 />
-
               )}
 
-              { this.state.popup === 'gallery' && (
+              {this.state.popup === "gallery" && (
                 <Gallery
                   onConfirm={this.confirmGallerySelection.bind(this)}
                   apiEndpoint={this.props.churchGraphicEndpoint}
@@ -231,16 +238,14 @@ class ImageUploader extends Component {
               <button
                 className="r_popup__btn--close"
                 onClick={this.onClosePopup.bind(this)}
-                >
+              >
                 Cancel
               </button>
-
             </div>
           )}
         </ReactCSSTransitionGroup>
 
         <div className="form__field">
-
           <div
             className="imageUpload__dropzone form__input"
             onDragEnter={this.handleDragEnter.bind(this)}
@@ -248,68 +253,60 @@ class ImageUploader extends Component {
             onDragOver={this.handleDragOver.bind(this)}
             onDrop={this.handleDrop.bind(this)}
           >
-
-            { this.state.previewURL && (
+            {this.state.previewURL && (
               <img
                 alt=""
                 className="imageUpload__imagePreview"
-                src={this.state.previewURL} 
+                src={this.state.previewURL}
               />
             )}
 
             <input
               className="imageUpload__uploadField"
-              id='dropzone-file-field'
+              id="dropzone-file-field"
               onChange={this.handleDrop.bind(this)}
               type="file"
             />
 
-            { this.props.churchGraphicEndpoint && (
-              <span>
-                <button
-                  className="btn"
-                  onClick={this.selectExisting.bind(this)}
-                >
-                  Select existing lead image
-                </button>
-                <span className="imageUpload__btn_pad">or</span>
-              </span>
-            )}
+            <div className="imageUpload__btnGroup">
+              {this.props.churchGraphicEndpoint && (
+                <Fragment>
+                  <button
+                    className="btn"
+                    onClick={this.selectExisting.bind(this)}
+                  >
+                    Select
+                  </button>
+                  <span className="imageUpload__btn_pad">or</span>
+                </Fragment>
+              )}
+
+              <label htmlFor="dropzone-file-field" className="btn">
+                Upload
+              </label>
+            </div>
 
             <label
-              htmlFor="dropzone-file-field"
-              className="btn"
-            >
-              Upload
-            </label>
-
-            <label
-              className="btn"
+              className="btn btn--small"
               onClick={this.clearSelectedImage.bind(this)}
             >
               Clear selected
             </label>
-
           </div>
 
-          <label className="form__label">
-            { this.props.fieldLabel }
-          </label>
-
+          <label className="form__label">{this.props.fieldLabel}</label>
         </div>
 
-        {this.state.errors.length > 0 && (
-          <Toast messages={this.state.errors} />
-        )}
+        {this.state.errors.length > 0 && <Toast messages={this.state.errors} />}
       </div>
     );
   }
 }
 
 ImageUploader.defaultProps = {
-  validTypes: 'image.jpg|image.jpeg|image.png',
-  fieldLabel: 'Lead image'
-}
+  validTypes: "image.jpg|image.jpeg|image.png",
+  fieldLabel: "Lead image"
+};
 
 ImageUploader.propTypes = {
   previewURL: PropTypes.string,
@@ -317,12 +314,14 @@ ImageUploader.propTypes = {
   validTypes: PropTypes.string,
   fieldLabel: PropTypes.string,
   churchGraphicEndpoint: PropTypes.string
-}
+};
 
 export default function(selector) {
   const container = document.querySelector(selector);
-  if (!container) { return; }
-  
+  if (!container) {
+    return;
+  }
+
   function callback(imageRef) {
     const field = document.getElementById(container.dataset.inputid);
     field.value = imageRef;
@@ -334,6 +333,7 @@ export default function(selector) {
       fieldLabel={container.dataset.fieldlabel}
       churchGraphicEndpoint={container.dataset.churchgraphicendpoint}
       onConfirm={callback}
-    />
-  , container);
+    />,
+    container
+  );
 }
